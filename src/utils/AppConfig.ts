cf4aaab29fc519a78eef6df04c0c3f56e5f59a46
Anna Sudol <sudolann@gmail.com>
2023-07-28 00:33:48 +0200

    chore(FE): edited AppConfig

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -2,6 +2,6 @@ export const AppConfig = {
   site_name: 'Triangle problem',
   title: 'Triangle problem',
   description:
-    'A NextJS app calculates digits from triangle moving only to one of the two adjacent numbers on the row below',
+    'A NextJS app calculates digits from triangle moving only to one of the two adjacent numbers on',
   locale: 'en',
 };
