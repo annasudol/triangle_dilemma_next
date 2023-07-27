b83cc72dd8db6f2b774204fa325bef7fc532c89b
Anna Sudol <sudolann@gmail.com>
2023-07-27 17:07:02 +0200

    fix(FE): build Error

--- page.tsx
+++ page.tsx
@@ -1,7 +1,5 @@
 'use client';
-import exampleTextFile from './Triangle';
 
-import Button from '@/components/Button';
 import UploadForm from '@/components/UploadForm';
 export default function Home() {
   return (
@@ -10,9 +8,7 @@ export default function Home() {
       <p className="text-white text-center">
         A web app that calculate the maximum total from top to bottom in a text file
       </p>
-      <Button href={exampleTextFile} variant="link">
-        Download .CSV
-      </Button>
+
       <UploadForm />
     </main>
   );
